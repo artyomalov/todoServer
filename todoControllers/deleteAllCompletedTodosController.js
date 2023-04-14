8ae260a7502cd9ddb41b13740e59611b140bcc7c
artyomalov <artyomalov@gmail.com>
2023-04-14 18:00:59 +0300

    commited delete

--- todoControllers/deleteAllCompletedTodosController.js
+++ todoControllers/deleteAllCompletedTodosController.js
@@ -2,6 +2,5 @@ const Todo = require('../models/todoModel');
 
 exports.deleteAllCompletedTodos = async (_, res) => {
   const result = await Todo.deleteMany({completed: true});
-  console.log(result);
   res.sendStatus(200);
 }
